35d7aca203e0143d5811845db65a93e8d34241e5
Thorsten Kober <tkober@gmail.com>
2023-06-27 12:32:03 -0700

    format output

--- dist/footer.constructable.js
+++ dist/footer.constructable.js
@@ -1,3 +1,29 @@
 const sheet = new CSSStyleSheet();
-sheet.replaceSync(":host {\n    display: block;\n\tbox-shadow: none !important;\n}\n\n.footer {\n\tmargin: 65px auto 0;\n\tcolor: #4d4d4d;\n\tfont-size: 11px;\n\ttext-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);\n\ttext-align: center;\n}\n\n.footer-text {\n\tline-height: 1;\n}\n\n.footer-link {\n\tcolor: inherit;\n\ttext-decoration: none;\n\tfont-weight: 400;\n}\n\n.footer-link:hover {\n\ttext-decoration: underline;\n}\n");
+sheet.replaceSync(`:host {
+    display: block;
+	box-shadow: none !important;
+}
+
+.footer {
+	margin: 65px auto 0;
+	color: #4d4d4d;
+	font-size: 11px;
+	text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+	text-align: center;
+}
+
+.footer-text {
+	line-height: 1;
+}
+
+.footer-link {
+	color: inherit;
+	text-decoration: none;
+	font-weight: 400;
+}
+
+.footer-link:hover {
+	text-decoration: underline;
+}
+`);
 export default sheet;
--- dist/header.constructable.js
+++ dist/header.constructable.js
@@ -1,3 +1,24 @@
 const sheet = new CSSStyleSheet();
-sheet.replaceSync(":host {\n    display: block;\n\tbox-shadow: none !important;\n}\n\n.header {\n\tmargin-top: 27px;\n}\n\n.title {\n\twidth: 100%;\n\tfont-size: 80px;\n\tline-height: 80px;\n\tmargin: 0;\n\tfont-weight: 200;\n\ttext-align: center;\n\tcolor: #b83f45;\n\t-webkit-text-rendering: optimizeLegibility;\n\t-moz-text-rendering: optimizeLegibility;\n\ttext-rendering: optimizeLegibility;\n}\n");
+sheet.replaceSync(`:host {
+    display: block;
+	box-shadow: none !important;
+}
+
+.header {
+	margin-top: 27px;
+}
+
+.title {
+	width: 100%;
+	font-size: 80px;
+	line-height: 80px;
+	margin: 0;
+	font-weight: 200;
+	text-align: center;
+	color: #b83f45;
+	-webkit-text-rendering: optimizeLegibility;
+	-moz-text-rendering: optimizeLegibility;
+	text-rendering: optimizeLegibility;
+}
+`);
 export default sheet;
